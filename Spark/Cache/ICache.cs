36368624f61a4b37008b8854c07d122150853d86
Taylor Stark <taylor.stark03@gmail.com>
2017-07-29 20:34:23 -0700

    Add memory cache

---
+++ Spark/Cache/ICache.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Spark.Cache
+{
+    public interface ICache<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        uint Count { get; }
+        bool IsEmpty { get; }
+        ICollection<TKey> Keys { get; }
+        ICollection<TValue> Values { get; }
+
+        void Add(TKey key, TValue value);
+        bool Contains(TKey key);
+        bool Get(TKey key, out TValue value);
+        bool Remove(TKey key);
+    }
+}
---
+++ Spark/Cache/Memory/MemoryCache.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Spark.Cache.Memory
+{
+    public class MemoryCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        private ConcurrentDictionary<TKey, TValue> Cache { get; }
+
+        public uint Count => (uint)Cache.Count;
+
+        public bool IsEmpty => Cache.IsEmpty;
+
+        public ICollection<TKey> Keys => Cache.Keys;
+
+        public ICollection<TValue> Values => Cache.Values;
+
+        public MemoryCache()
+        {
+            Cache = new ConcurrentDictionary<TKey, TValue>();
+        }
+
+        public MemoryCache(IEqualityComparer<TKey> comparer)
+        {
+            Cache = new ConcurrentDictionary<TKey, TValue>(comparer);
+        }
+
+        public MemoryCache(int concurrencyLevel, int capacity)
+        {
+            Cache = new ConcurrentDictionary<TKey, TValue>(concurrencyLevel, capacity);
+        }
+
+        public MemoryCache(int concurrencyLevel, int capacity, IEqualityComparer<TKey> comparer)
+        {
+            Cache = new ConcurrentDictionary<TKey, TValue>(concurrencyLevel, capacity, comparer);
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return Cache.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Cache[key] = value;
+        }
+
+        public bool Contains(TKey key)
+        {
+            return Cache.ContainsKey(key);
+        }
+
+        public bool Get(TKey key, out TValue value)
+        {
+            return Cache.TryGetValue(key, out value);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return Cache.TryRemove(key, out TValue _);
+        }
+    }
+}
