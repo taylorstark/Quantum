623a651ea3bf040b2ec7506aad6032402e96ee9c
Taylor Stark <taylor.stark03@gmail.com>
2017-07-29 21:05:24 -0700

    Add clear extension to cache

---
+++ Spark.Test/Cache/CacheExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using Spark.Cache;
+using Xunit;
+
+namespace Spark.Test.Cache
+{
+    public class CacheExtensionsTests
+    {
+        [Fact]
+        public void ClearEmptyCache()
+        {
+            var cache = Substitute.For<ICache<uint, uint>>();
+            cache.Keys.Returns(new List<uint>());
+            cache.Remove(Arg.Any<uint>()).Returns(true);
+
+            cache.Clear();
+
+            cache.DidNotReceive().Remove(Arg.Any<uint>());
+        }
+
+        [Fact]
+        public void ClearCache()
+        {
+            var cache = Substitute.For<ICache<uint, uint>>();
+            cache.Keys.Returns(new List<uint> { 1, 2 });
+            cache.Remove(Arg.Any<uint>()).Returns(true);
+
+            cache.Clear();
+
+            cache.Received().Remove(Arg.Is(1U));
+            cache.Received().Remove(Arg.Is(2U));
+        }
+    }
+}
---
+++ Spark/Cache/CacheExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Spark.Cache
+{
+    public static class CacheExtensions
+    {
+        public static void Clear<TKey, TValue>(this ICache<TKey, TValue> cache)
+        {
+            foreach (var key in cache.Keys)
+            {
+                cache.Remove(key);
+            }
+        }
+    }
+}
